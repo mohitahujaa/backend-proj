d1804d043ee9cbcbe1dbbdad719fa5d7627c8a08
Mohit Ahuja <mohitahuja720@gmail.com>
2026-08-03 19:00:57 +0530

    renamed the subscribe router to channel router as more natural for api designing

--- src/routes/channel.route.js
+++ src/routes/channel.route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { toggleSubscribe } from "../controllers/subscriber.controller.js";
+import { getSubscribedChannels, toggleSubscribe } from "../controllers/subscriber.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
 
-router.route('/:channel').post(verifyJWT, toggleSubscribe);
+router.route('/:channelUsername/subscribe').post(verifyJWT, toggleSubscribe);
 
 export default router;
